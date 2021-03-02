350f39fea416fa5bfd1b2ddbd605e6b3128e70cb
Christian Meißner <cme+github@meissner.sh>
2021-03-02 10:20:16 +0100

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='p3exporter',
-    version='0.0.1',
+    version='0.0.2',
     description='Python Programmable Prometheus exporter',
     long_description=long_description,
     long_description_content_type='text/markdown',
