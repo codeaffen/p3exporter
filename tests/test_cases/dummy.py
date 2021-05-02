25e11296c2ea5aa2af1c7e699de8795e4e1febcc
Christian Meißner <cme+github@meissner.sh>
2021-05-02 21:04:08 +0200

    Start real life test development
    * Update conftest for our purpose
      Strip unused functions and fixtures. We start to collect our own
      functions from now on.
    * Create real test case
      Remove dummy test and add first test case for example collector. We need
      to workout more tests for all other collectors later.
    * Run test also on pull requests
      Currently `CI` workflow is only run on push. We decided to run this
      from now on also on pull requests.

--- tests/test_cases/dummy.py
+++ tests/test_cases/dummy.py
@@ -1,14 +1,10 @@
 """Test controller method."""
-import vcr
-
-from tests.conftest import filter_request_uri, filter_response, cassette_name, FILTER_REQUEST_HEADERS
+from tests.conftest import get_case_name, create_config
 
 
-@vcr.use_cassette(cassette_name('test_dummy'),
-                  filter_headers=FILTER_REQUEST_HEADERS,
-                  before_record_request=filter_request_uri,
-                  before_recorde_response=filter_response
-                  )
+test_case = get_case_name(__file__)
+collector_config = create_config(test_case)
+
 def test_dummy():
     """Test if controllers method returns correct datatype."""
     assert 1 == 1
